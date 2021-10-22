066584c23a3f8ca026452bddec30d5d800bf5a1a
kbanchi1111 <kbanchi.1111@gmail.com>
2021-10-22 18:04:49 +0900

    add show page

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -54,6 +54,12 @@
                                 {{ __('Manage Account') }}
                             </div>
 
+                            @if (Auth::guard(CompanyConst::GUARD)->check())
+                                <x-jet-dropdown-link href="{{ route('job_offers.create') }}">
+                                    {{ '求人情報登録' }}
+                                </x-jet-dropdown-link>
+                            @endif
+
                             <x-jet-dropdown-link href="{{ route($prefix . 'profile.show') }}">
                                 {{ __('Profile') }}
                             </x-jet-dropdown-link>
@@ -127,6 +133,12 @@
                     :active="request()->routeIs($prefix . 'profile.show')">
                     {{ __('Profile') }}
                 </x-jet-responsive-nav-link>
+
+                @if (Auth::guard(CompanyConst::GUARD)->check())
+                    <x-jet-dropdown-link href="{{ route('job_offers.create') }}">
+                        {{ '求人情報登録' }}
+                    </x-jet-dropdown-link>
+                @endif
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route($prefix . 'logout') }}">
