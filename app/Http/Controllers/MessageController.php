3fb2dd18d18a11cb17309fe4ef052d8f268a3a39
kbanchi1111 <kbanchi.1111@gmail.com>
2021-10-23 21:46:41 +0900

    add message page

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Consts\CompanyConst;
+use App\Consts\MessageConst;
+use App\Consts\UserConst;
+use App\Models\Company;
+use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Models\JobOffer;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class MessageController extends Controller
+{
+    public function index(JobOffer $jobOffer, User $user)
+    {
+
+        $query = Message::query();
+        $messages = $query
+            ->where('job_offer_id', $jobOffer->id)
+            ->where('user_id', $user->id)
+            ->get()->sortBy('created_at');
+
+        $sender = '';
+        $partner = '';
+        $send_by = '';
+        if (Auth::guard(UserConst::GUARD)->check()) {
+            $sender = $user;
+            $partner = $jobOffer->company;
+            $send_by = MessageConst::SEND_BY_USER;
+        }
+        if (Auth::guard(CompanyConst::GUARD)->check()) {
+            $sender = $jobOffer->company;
+            $partner = $user;
+            $send_by = MessageConst::SEND_BY_COMPANY;
+        }
+
+        return view('messages.index', compact('jobOffer', 'messages', 'sender', 'partner', 'send_by'));
+    }
+
+    public function store(Request $request, JobOffer $jobOffer, User $user)
+    {
+        $message = new Message();
+        $message->message = $request->message;
+        $message->job_offer_id = $jobOffer->id;
+        if (Auth::guard(UserConst::GUARD)->check()) {
+            $message->user_id = $user->id;
+            $message->send_by = MessageConst::SEND_BY_USER;
+        }
+        if (Auth::guard(CompanyConst::GUARD)->check()) {
+            $message->user_id = $jobOffer->company->id;
+            $message->send_by = MessageConst::SEND_BY_COMPANY;
+        }
+
+        DB::beginTransaction();
+        try {
+            $message->save();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            return back()->withInput()
+                ->withErrors('エラーが発生しました');
+        }
+
+        return redirect()
+            ->route('job_offers.users.messages.index', [$jobOffer, $user])
+            ->with('notice', 'Send Message');
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -61,7 +62,7 @@ class UserFactory extends Factory
 
         return $this->has(
             Team::factory()
-                ->state(function (array $attributes, User $user) {
+                ->state(function (array $attributes, Company $user) {
                     return ['name' => $user->name . '\'s Team', 'user_id' => $user->id, 'personal_team' => true];
                 }),
             'ownedTeams'
